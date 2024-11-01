b93e858dbad3e98cf237bdf1c0d6fccd49a8d679
IcaroCos19 <icarkz219@gmail.com>
2024-11-01 12:25:23 -0300

    Service layer, component registration

---
+++ src/main/java/com/educa/course/services/UserService.java
@@ -0,0 +1,27 @@
+package com.educa.course.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.educa.course.entities.User;
+import com.educa.course.repositories.UserRepository;
+
+@Service
+public class UserService  {
+	
+	
+	@Autowired
+	private UserRepository repository;
+	
+	public List<User> findAll(){
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj =repository.findById(id);
+		return obj.get();
+	}
+}	
